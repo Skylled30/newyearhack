38f2ffde42e8abff92f7674e8dc9337047110e33
Mikhail Mustakimov <mikhail@mustakimov.ru>
2017-12-23 22:26:20 +0800

    Refactor

--- project1_cam.py
+++ project1_cam.py
@@ -2,19 +2,32 @@ import cv2
 import numpy as np
 import math
 
-# cap = cv2.VideoCapture(0)
-
-# image = cv2.imread('14.jpg')
-
-cv2.namedWindow('Pich', cv2.WINDOW_KEEPRATIO)
-cv2.namedWindow('Persp', cv2.WINDOW_KEEPRATIO)
-cv2.namedWindow('Cont_Persp', cv2.WINDOW_KEEPRATIO)
-# cv2.namedWindow('Finish', cv2.WINDOW_KEEPRATIO)
-
+colors_arr = [
+    {
+        "type": "red",
+        "colors": {
+            "min": [164, 119, 67],
+            "max": [184, 139, 87]
+        }
+    },
+    {
+        "type": "yellow",
+        "colors": {
+            "min": [8, 183, 114],
+            "max": [28, 203, 134]
+        }
+    },
+    {
+        "type": "green",
+        "colors": {
+            "min": [70, 93, 53],
+            "max": [100, 113, 73]
+        }
+    }
+]
 positions = []
+sqrt_mass = []
 trans = np.array([[0, 0], [1080, 1920]], dtype='float32')
-
-cap = cv2.VideoCapture(1)
 
 
 def on_mouse_click(event, x, y, flags, param):
@@ -24,10 +37,7 @@ def on_mouse_click(event, x, y, flags, param):
             positions += [[x, y]]
 
 
-cv2.setMouseCallback("Pich", on_mouse_click)
-
-while cap.isOpened():
-    image = cap.read()[1]
+def transform_image(image: np.ndarray, positions: list) -> (bool, np.ndarray):
     if len(positions) == 4:
         rect = np.zeros((4, 2), dtype="float32")
         rect[0] = positions[0]
@@ -53,10 +63,55 @@ while cap.isOpened():
 
         M = cv2.getPerspectiveTransform(rect, dst)
         img_persp = cv2.warpPerspective(image, M, (max_width, max_height))
+        return True, img_persp
+    return False, None
 
-        gray_persp = cv2.cvtColor(img_persp, cv2.COLOR_BGR2GRAY)
+
+def get_colors(rect: np.ndarray, colors) -> list:
+    result = list()
+    height = rect.shape[0]
+    width = rect.shape[1]
+    for i in range(2):
+        top = height * i + height // 4
+        bottom = height * (i + 1) - height // 4
+        for j in range(2):
+            left = width * j + width // 4
+            right = width * (j + 1) - width // 4
+            color_mat = rect[top:bottom, left:right]
+            print("_______")
+            print(color_mat)
+            print("-------")
+            color_mat = [
+                color_mat[0].mean(),
+                color_mat[1].mean(),
+                color_mat[2].mean()
+            ]
+            print(color_mat)
+            print("_______")
+            for color in colors:
+                min_colors = color["colors"]["min"]
+                max_colors = color["colors"]["max"]
+                print(min_colors)
+                print(max_colors)
+                print(color_mat)
+
+                if (
+                        min_colors[0] < color_mat[0] < max_colors[0] and
+                        min_colors[1] < color_mat[1] < max_colors[1] and
+                        min_colors[2] < color_mat[2] < max_colors[2]
+                ):
+                    result += color["type"]
+    if len(result) != 4:
+        return []
+    return result
+
+
+def handle_image(image: np.ndarray):
+    global colors_arr
+    succeed, img_persp = transform_image(image, positions)
+
+    if succeed:
         img_persp = cv2.GaussianBlur(img_persp, (5, 5), 0)
-        # gray_persp = cv2.Laplacian(gray_persp, 3)
         canny_persp = cv2.Canny(img_persp, 1, 100)
 
         contours = cv2.findContours(canny_persp.copy(),
@@ -65,13 +120,31 @@ while cap.isOpened():
 
         max_area = cv2.contourArea(max(contours, key=cv2.contourArea))
         square_length = int(math.sqrt(max_area))
+        if len(sqrt_mass) < 20:
+            sqrt_mass.append(square_length)
+        else:
+            max_width = img_persp.shape[1]
+            max_height = img_persp.shape[0]
+            square_length = int(np.array(sqrt_mass).mean())
 
-        space_length = int(square_length * 0.203125 * 1.35)
-        width_count = int(max_width / (square_length + space_length))
-        height_count = int(max_height / (square_length + space_length))
+            space_length = int(square_length * 0.203125 * 1.2)
+            width_count = int(max_width / (square_length + space_length))
+            height_count = int(max_height / (square_length + space_length))
 
-        for i in range(1, width_count):
-            cv2.line(img_persp, (i*(square_length+space_length), 0), (i*(square_length+space_length), max_height), (0, 255, 0), 3)
+            for i in range(1, width_count + 1):
+                left = (i - 1) * (square_length + space_length)
+                right = i * (square_length + space_length)
+                for j in range(1, height_count + 1):
+                    top = (j - 1) * (square_length + space_length)
+                    bottom = j * (square_length + space_length)
+
+                    rect = cv2.cvtColor(img_persp[top:bottom, left:right], cv2.COLOR_BGR2HSV)
+
+                    colors = get_colors(rect, colors_arr)
+                    print(i, j, colors)
+
+                cv2.line(img_persp, (i * (square_length + space_length), 0),
+                         (i * (square_length + space_length), max_height), (0, 255, 0), 3)
 
         for contour in contours:
             if (cv2.contourArea(contour) + max_area * 0.5) >= max_area:
@@ -84,10 +157,28 @@ while cap.isOpened():
                     0.7, (0, 0, 255), 2)
 
     cv2.imshow('Pich', image)
-    key = cv2.waitKey(1)
-    if key == ord('q'):
-        break
 
-cv2.destroyAllWindows()
-cap.release()
-# cv2.imshow('Contours',)
+
+def main():
+    cv2.namedWindow('Pich', cv2.WINDOW_KEEPRATIO)
+    cv2.namedWindow('Persp', cv2.WINDOW_KEEPRATIO)
+    cv2.namedWindow('Cont_Persp', cv2.WINDOW_KEEPRATIO)
+
+    cap = cv2.VideoCapture(1)
+    cv2.setMouseCallback("Pich", on_mouse_click)
+
+    while cap.isOpened():
+        image = cap.read()[1]
+
+        handle_image(image)
+
+        key = cv2.waitKey(1)
+        if key == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
+    cap.release()
+
+
+if __name__ == '__main__':
+    main()
